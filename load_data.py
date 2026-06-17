7bda46be31b7bcee4fc1f66998aed25a9a258ef8
bigbang123789789 <134249895+bigbang123789789@users.noreply.github.com>
2026-06-17 12:20:51 +0700

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -1,98 +1,161 @@
 import numpy as np
-import os
-import pickle
+from sklearn.utils import shuffle
+
 
 def load_data_shuffle(cv):
 
-    train_pos_save = "data/VSFC/Data_not_token/Fold_"+ str(cv)+"/train_pos.npy"
-    train_neg_save = "data/VSFC/Data_not_token/Fold_"+ str(cv)+"/train_neg.npy"
-    train_neu_save = "data/VSFC/Data_not_token/Fold_"+ str(cv)+"/train_neu.npy"
 
-    test_pos_save = "data/VSFC/Data_not_token/Fold_"+ str(cv)+"/test_pos.npy"
-    test_neg_save = "data/VSFC/Data_not_token/Fold_"+ str(cv)+"/test_neg.npy"
-    test_neu_save = "data/VSFC/Data_not_token/Fold_"+ str(cv)+"/test_neu.npy"
-
+    base = (
+        "data/VSFC/Data_not_token/Fold_"
+        + str(cv)
+        + "/"
+    )
 
 
+    # ==================
+    # LOAD
+    # ==================
 
-    #Load train data
-    pos_train = np.load(train_pos_save)
-    neg_train = np.load(train_neg_save)
-    neu_train = np.load(train_neu_save)
-
-    y_pos_train = []
-    for i in pos_train:
-        y_pos_train.append([1,0,0])
-    y_pos_train = np.array(y_pos_train)
-
-    y_neg_train = []
-    for i in neg_train:
-        y_neg_train.append([0, 1, 0])
-    y_neg_train = np.array(y_neg_train)
-
-    y_neu_train = []
-    for i in neu_train:
-        y_neu_train.append([0, 0, 1])
-    y_neu_train = np.array(y_neu_train)
+    pos_train = np.load(base+"train_pos.npy")
+    neg_train = np.load(base+"train_neg.npy")
+    neu_train = np.load(base+"train_neu.npy")
 
 
-    #load test data
-    pos_test = np.load(test_pos_save)
-    neg_test = np.load(test_neg_save)
-    neu_test = np.load(test_neu_save)
-
-    y_pos_test = []
-    for i in pos_test:
-        y_pos_test.append([1,0,0])
-    y_pos_test = np.array(y_pos_test)
-
-    y_neg_test = []
-    for i in neg_test:
-        y_neg_test.append([0, 1, 0])
-    y_neg_test = np.array(y_neg_test)
-
-    y_neu_test = []
-    for i in neu_test:
-        y_neu_test.append([0, 0, 1])
-    y_neu_test = np.array(y_neu_test)
-
-
-    #split train and validate set
-    val_len = len(pos_train)//10
-
-    pos_val = pos_train[0:val_len]
-    pos_train= pos_train[val_len:]
-    y_pos_val = y_pos_train[0:val_len]
-    y_pos_train = y_pos_train[val_len:]
-
-
-    neg_val = neg_train[0:val_len]
-    neg_train = neg_train[val_len:]
-    y_neg_val = y_neg_train[0:val_len]
-    y_neg_train = y_neg_train[val_len:]
-
-
-    neu_val = neu_train[0:val_len]
-    neu_train = neu_train[val_len:]
-    y_neu_val = y_neu_train[0:val_len]
-    y_neu_train = y_neu_train[val_len:]
+    pos_test = np.load(base+"test_pos.npy")
+    neg_test = np.load(base+"test_neg.npy")
+    neu_test = np.load(base+"test_neu.npy")
 
 
 
- 
+    # ==================
+    # LABEL
+    # ==================
+
+    y_pos_train = np.tile(
+        [1,0,0],
+        (len(pos_train),1)
+    )
 
 
-    X_train = np.concatenate([pos_train,neu_train,neg_train])
-    y_train = np.concatenate([y_pos_train,y_neu_train,y_neg_train])
+    y_neg_train = np.tile(
+        [0,1,0],
+        (len(neg_train),1)
+    )
 
-    X_val = np.concatenate([pos_val,neu_val,neg_val])
-    y_val = np.concatenate([y_pos_val,y_neu_val,y_neg_val])
 
-    X_test = np.concatenate([pos_test,neu_test,neg_test])
-    y_test = np.concatenate([y_pos_test,y_neu_test,y_neg_test])
+    y_neu_train = np.tile(
+        [0,0,1],
+        (len(neu_train),1)
+    )
 
-    # print X_train.shape, y_train.shape
-    # print X_test.shape, y_test.shape
-    # print X_val.shape, y_val.shape
 
-    return X_train, y_train, X_test, y_test, X_val, y_val
+
+    y_pos_test = np.tile(
+        [1,0,0],
+        (len(pos_test),1)
+    )
+
+
+    y_neg_test = np.tile(
+        [0,1,0],
+        (len(neg_test),1)
+    )
+
+
+    y_neu_test = np.tile(
+        [0,0,1],
+        (len(neu_test),1)
+    )
+
+
+
+    # ==================
+    # MERGE TRAIN
+    # ==================
+
+    X = np.concatenate(
+        [
+            pos_train,
+            neg_train,
+            neu_train
+        ]
+    )
+
+
+    y = np.concatenate(
+        [
+            y_pos_train,
+            y_neg_train,
+            y_neu_train
+        ]
+    )
+
+
+    # shuffle toàn bộ
+
+    X, y = shuffle(
+        X,
+        y,
+        random_state=42
+    )
+
+
+
+    # ==================
+    # VALIDATION SPLIT
+    # ==================
+
+    split = int(
+        0.9 * len(X)
+    )
+
+
+    X_train = X[:split]
+
+    y_train = y[:split]
+
+
+    X_val = X[split:]
+
+    y_val = y[split:]
+
+
+
+    # ==================
+    # TEST
+    # ==================
+
+    X_test = np.concatenate(
+        [
+            pos_test,
+            neg_test,
+            neu_test
+        ]
+    )
+
+
+    y_test = np.concatenate(
+        [
+            y_pos_test,
+            y_neg_test,
+            y_neu_test
+        ]
+    )
+
+
+    X_test, y_test = shuffle(
+        X_test,
+        y_test,
+        random_state=42
+    )
+
+
+
+    return (
+        X_train,
+        y_train,
+        X_test,
+        y_test,
+        X_val,
+        y_val
+    )

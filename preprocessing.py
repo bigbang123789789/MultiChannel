71e3d4da22db959358e18d5a851a29b4c3bd1a51
bigbang123789789 <134249895+bigbang123789789@users.noreply.github.com>
2026-06-17 11:59:50 +0700

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,71 +1,171 @@
 import numpy as np
-import re
-import numpy
+import os
 
-	
-cvs = [1,2,3,4,5]
+
+# độ dài câu đầu vào cho CNN/LSTM
+MAX_LEN = 100
+
+
+cvs = [1]
+
 for cv in cvs:
-	word2id= {}
-	id2word={}
-	index = 1
-	maxlen = 0
-	avglen = 0
-	count100 = 0
-	#read file
-	train_pos_file = "data/VS/Data_not_token/Fold_"+ str(cv)+"/train_nhan_1.txt"
-	train_neg_file = "data/VS/Data_not_token/Fold_"+ str(cv)+"/train_nhan_0.txt"
-	train_neu_file = "data/VS/Data_not_token/Fold_"+ str(cv)+"/train_nhan_2.txt"
 
-	test_pos_file = "data/VS/Data_not_token/Fold_"+ str(cv)+"/test_nhan_1.txt"
-	test_neg_file = "data/VS/Data_not_token/Fold_"+ str(cv)+"/test_nhan_0.txt"
-	test_neu_file = "data/VS/Data_not_token/Fold_"+ str(cv)+"/test_nhan_2.txt"
+    word2id = {}
+    id2word = {}
+    index = 1
+
+    maxlen = 0
+    avglen = 0
+    count100 = 0
 
 
+    # ======================
+    # INPUT FILE
+    # ======================
 
-	open_files = [train_pos_file, train_neg_file, train_neu_file, test_pos_file, test_neg_file, test_neu_file]
+    train_pos_file = f"data/VSFC/Data_not_token/Fold_{cv}/train_pos.txt"
+    train_neg_file = f"data/VSFC/Data_not_token/Fold_{cv}/train_neg.txt"
+    train_neu_file = f"data/VSFC/Data_not_token/Fold_{cv}/train_neu.txt"
+    
+    test_pos_file = f"data/VSFC/Data_not_token/Fold_{cv}/test_pos.txt"
+    test_neg_file = f"data/VSFC/Data_not_token/Fold_{cv}/test_neg.txt"
+    test_neu_file = f"data/VSFC/Data_not_token/Fold_{cv}/test_neu.txt"
 
-	#save file
-	train_pos_save = "data/VS/Data_not_token/Fold_"+ str(cv)+"/train_pos"
-	train_neg_save = "data/VS/Data_not_token/Fold_"+ str(cv)+"/train_neg"
-	train_neu_save = "data/VS/Data_not_token/Fold_"+ str(cv)+"/train_neu"
+    open_files = [
+        train_pos_file,
+        train_neg_file,
+        train_neu_file,
+        test_pos_file,
+        test_neg_file,
+        test_neu_file
+    ]
 
-	test_pos_save = "data/VS/Data_not_token/Fold_"+ str(cv)+"/test_pos"
-	test_neg_save = "data/VS/Data_not_token/Fold_"+ str(cv)+"/test_neg"
-	test_neu_save = "data/VS/Data_not_token/Fold_"+ str(cv)+"/test_neu"
 
-	save_files = [train_pos_save, train_neg_save, train_neu_save, test_pos_save, test_neg_save, test_neu_save]
+    # ======================
+    # OUTPUT FILE
+    # ======================
 
-	for open_file, save_file in zip(open_files,save_files):
-		pos = []
-		file = open(open_file, 'r')
+    save_files = [
+        f"data/VSFC/Data_not_token/Fold_{cv}/train_pos.npy",
+        f"data/VSFC/Data_not_token/Fold_{cv}/train_neg.npy",
+        f"data/VSFC/Data_not_token/Fold_{cv}/train_neu.npy",
+        f"data/VSFC/Data_not_token/Fold_{cv}/test_pos.npy",
+        f"data/VSFC/Data_not_token/Fold_{cv}/test_neg.npy",
+        f"data/VSFC/Data_not_token/Fold_{cv}/test_neu.npy"
+    ]
 
-		for aline in file.readlines():
-		    aline = aline.replace('\n', "")
-		    ids = np.array([], dtype='int32')
-		    for word in aline.split(' '):
-		        word = word.lower()
-		        if word in word2id:
-		            ids = np.append(ids, word2id[word])
-		        else:
-		            if word != '':
-		                # print (word, "not in vocalbulary")
-		                word2id[word] = index
-		                id2word[index] = word
-		                ids = np.append(ids, index)
-		                index = index + 1
-		    if len(ids) > 0:
-		        pos.append(ids)
 
-		file.close()
-		print len(pos)
-		np.save(save_file, pos)
-		for li in pos:
-		    if maxlen < len(li):
-		        maxlen = len(li)
-		    avglen += len(li)
-		    if len(li) > 250:
-		        count100+=1
 
-	print len(word2id)
-	print ("maxlen",maxlen)
-	print ("maxlen250",count100)
+    # ======================
+    # PROCESS DATA
+    # ======================
+
+    for open_file, save_file in zip(open_files, save_files):
+
+        data = []
+
+
+        with open(open_file, 'r', encoding='utf-8') as file:
+
+            lines = file.readlines()
+
+
+            for aline in lines:
+
+                aline = aline.strip()
+
+
+                ids = []
+
+
+                for word in aline.split(' '):
+
+                    word = word.lower().strip()
+
+
+                    if word == '':
+                        continue
+
+
+                    # có trong vocab
+                    if word in word2id:
+
+                        ids.append(word2id[word])
+
+
+                    # từ mới
+                    else:
+
+                        word2id[word] = index
+                        id2word[index] = word
+
+                        ids.append(index)
+
+                        index += 1
+
+
+
+                if len(ids) > 0:
+
+
+                    # ======================
+                    # PADDING
+                    # ======================
+
+                    if len(ids) < MAX_LEN:
+
+                        ids = ids + [0] * (MAX_LEN - len(ids))
+
+
+                    else:
+
+                        ids = ids[:MAX_LEN]
+
+
+
+                    data.append(ids)
+
+
+
+        data = np.array(
+            data,
+            dtype=np.int32
+        )
+
+
+        print(
+            save_file,
+            data.shape
+        )
+
+
+        np.save(
+            save_file,
+            data
+        )
+
+
+        # statistic
+
+        for li in data:
+
+            length = np.count_nonzero(li)
+
+            if maxlen < length:
+                maxlen = length
+
+
+            avglen += length
+
+
+            if length > 250:
+                count100 += 1
+
+
+
+    print("====================")
+    print("Fold:",cv)
+    print("Vocabulary:",len(word2id))
+    print("maxlen:",maxlen)
+    print("maxlen250:",count100)
+    print("====================")
